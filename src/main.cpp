75990781fcee433ff340816795f1a9d9b6a8ae13
Nurislam Iarmukhamedov <nuryarmukhamedov@mail.ru>
2025-07-31 15:21:45 +0300

    Added raw buttons and CommandAddition, FileReader funcitons

--- src/main.cpp
+++ src/main.cpp
@@ -4,79 +4,89 @@
 #include <vector>
 #include <string>
 #include <unordered_map>
+#include <sqlite3.h>
+
+void CommandAddition(TgBot::Bot& mybot , std::vector<TgBot::BotCommand::Ptr>& cmdstorage, const std::string title , const std::string descrp);
+void FileReader(std::ostringstream& buffer , const std::string path);
+
+template<typename T>
+bool in(const T& value , std::initializer_list<T> list)
+{
+    return std::find( list.begin() , list.end() , value) != list.end()  ;
+}
+
 enum class UserState {
-    None,
+    NONE,
     WAITING_TITLE,
-    WAITING_BODY
+    WAITING_BODY,
 };
-
-
-std::unordered_map<int64_t , bool> awaitingNote;
+std::unordered_map<int64_t , bool> awaitingText;
 std::unordered_map<int64_t , UserState> UserStates;
+
+// TODO Состояние ожидания у каждой команды
+
 int main() {
 
     std::ostringstream response;
-    std::ifstream helpfile;
-    try
-    {
-        helpfile.open( "../src/help_response.txt");
-        response << helpfile.rdbuf();
-        helpfile.close();
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << '\n';
-        return 1;
-    }
-    
-    std::string token;
-    std::ifstream tokenfile;
-    try
-    {
-        tokenfile.open("../build/token.txt");
-        tokenfile >> token;
-        tokenfile.close();
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << '\n';
-        return 1;
-    }
+    FileReader(response , "../src/help_response.txt");
+    std::ostringstream token;
+    FileReader(token , "../build/token.txt");
     
 
+    TgBot::Bot bot(token.str());
     std::vector<TgBot::BotCommand::Ptr> commands;
-
-    auto cmd1 = std::make_shared<TgBot::BotCommand>();
-    cmd1->command = "start";
-    cmd1->description = "Запустить бота";
-    commands.push_back(cmd1);
-
-    auto cmd2 = std::make_shared<TgBot::BotCommand>();
-    cmd2->command = "add";
-    cmd2->description = "Добавить заметку";
-    commands.push_back(cmd2);
-
-    // ... добавь остальные команды
-
-    TgBot::Bot bot(token);
-    
+    CommandAddition(bot , commands ,"start" , "Запустить бота");
+    CommandAddition(bot , commands ,"add" , "Добавить новую заметку");
+    CommandAddition(bot , commands , "list" , "Показать все заметки");
+    CommandAddition(bot , commands , "delete" , "Удалить заметку");
+    CommandAddition(bot , commands , "clear" , "Удалить все заметки");
+    CommandAddition(bot , commands , "help" , "Просмотр справки");
     bot.getApi().setMyCommands(commands);
+
+
+
+
+
+
     bot.getEvents().onCommand("start", [&bot , &response](TgBot::Message::Ptr msg) {
+        awaitingText[msg->chat->id] = false;
+        UserStates[msg->chat->id] = UserState::none;
         bot.getApi().sendMessage( msg->chat->id, "Привет,  "+ msg->chat->firstName +"!\nЯ бот-хранитель твоих заметок!");
-        awaitingNote.erase(msg->chat->id);
-        UserStates.erase(msg->chat->id);
         bot.getApi().sendMessage(msg->chat->id , response.str() );
     });
 
     bot.getEvents().onCommand("add" , [&bot](TgBot::Message::Ptr msg)
     {
-        awaitingNote[msg->chat->id] = true;
+        awaitingText[msg->chat->id] = true;
         bot.getApi().sendMessage(msg->chat->id, "Напиши заголовок заметки");
         UserStates[msg->chat->id] = UserState::WAITING_TITLE;
     });
 
+    bot.getEvents().onCommand("list" , [&bot](TgBot::Message::Ptr msg){
+        bot.getApi().sendMessage(msg->chat->id , "test txt");
+    });
+
+    bot.getEvents().onCommand("delete" , [&bot](TgBot::Message::Ptr msg){
+        bot.getApi().sendMessage(msg->chat->id , "Выбери заметку, которую хочешь удалить");
+    });
+
+    bot.getEvents().onCommand("clear" , [&bot](TgBot::Message::Ptr msg){
+        bot.getApi().sendMessage(msg->chat->id , "test txt");
+    });
+
+    bot.getEvents().onCommand("help" , [&bot , &response](TgBot::Message::Ptr msg){
+        bot.getApi().sendMessage(msg->chat->id , response.str() );
+    });
+
     bot.getEvents().onAnyMessage([&bot , &commands](TgBot::Message::Ptr msg) {
-        if( awaitingNote[msg->chat->id ] )
+        for( const auto& cmd : commands)
+        {
+            if( "/" + cmd->command == msg->text)
+            {
+                return;
+            }
+        }
+        if( awaitingText[msg->chat->id ] == true && in(UserStates[msg->chat->id] , {UserState::WAITING_TITLE , UserState::WAITING_BODY} ) )
         {
             std::string text;
             if( UserStates[msg->chat->id] == UserState::WAITING_TITLE )
@@ -89,23 +99,13 @@ int main() {
             {
                 text = msg->text;
                 bot.getApi().sendMessage(msg->chat->id , "Заметка сохранена");
-                UserStates.erase(msg->chat->id);
-                awaitingNote.erase(msg->chat->id);
+                UserStates[msg->chat->id] = UserState::NONE;
+                awaitingText[msg->chat->id] = false;
             }
         }
-        else
-        {
-            for( const auto& cmd : commands)
-            {
-            if( "/" + cmd->command == msg->text)
-            {
-                return;
-            }
-            }
-            bot.getApi().sendMessage( msg->chat->id , "Invalid command , use /help ");
-        }
+        bot.getApi().sendMessage( msg->chat->id , "Неизвестная команда, используйте /help ");
     });
-
+        
 
     try {
         TgBot::TgLongPoll longPoll(bot);
@@ -115,4 +115,28 @@ int main() {
         std::cerr << "Ошибка: " << e.what() << std::endl;
     }
     return 0;
+}
+
+void CommandAddition(TgBot::Bot& mybot , std::vector<TgBot::BotCommand::Ptr>& cmdstorage, const std::string title , const std::string descrp)
+{
+    auto command = std::make_shared<TgBot::BotCommand>();
+    command->command = title;
+    command->description = descrp;
+    cmdstorage.push_back(command);
+}
+
+void FileReader(std::ostringstream& buffer , const std::string path)
+{
+    std::ifstream file;
+    file.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+    try
+    {
+        file.open(path);
+        buffer << file.rdbuf();
+        file.close();
+    }
+    catch( const std::exception& e)
+    {
+        std::cerr << "Ошибка: " << e.what() << std::endl;
+    }
 }
